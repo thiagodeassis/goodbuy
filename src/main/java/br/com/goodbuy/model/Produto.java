2458693c479313638c1c2e133e39d98b0dbfb818
thiagodeassis <thiago.tfx@gmail.com>
2013-02-22 17:06:01 -0300

    Criado classe e mapeamento de Produtos

---
+++ src/main/java/br/com/goodbuy/model/Produto.java
@@ -0,0 +1,43 @@
+package br.com.goodbuy.model;
+
+import java.math.BigDecimal;
+
+public class Produto {
+	private Long id;
+	private String nome;
+	private String descricao;
+	private BigDecimal preco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+}
