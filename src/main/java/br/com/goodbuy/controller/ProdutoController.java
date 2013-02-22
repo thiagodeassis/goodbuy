e59b543ecfc218ec2a7d5c03c57699da848b34c1
thiagodeassis <thiago.tfx@gmail.com>
2013-02-22 11:33:38 -0300

    Criado primeiro Controller. Configurado web.xml do projeto. log4j.properties criado

--- src/main/java/br/com/goodbuy/controller/ProdutoController.java
+++ src/main/java/br/com/goodbuy/controller/ProdutoController.java
@@ -1,8 +1,17 @@
 package br.com.goodbuy.controller;
 
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 
 @Resource
+@Path("/produto")
 public class ProdutoController {
 
+	@Get
+	@Path("/")
+	public void paginaDePesquisa(){
+		
+	}
+	
 }
